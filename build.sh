543e471ffcfd054ddf1b64320c76fbe4027bc36b
Anupam Basak <anupam.basak27@gmail.com>
2018-04-26 18:54:39 +0530

    Use NXOS Testing Repository

--- build.sh
+++ build.sh
@@ -33,19 +33,14 @@ chroot filesystem/ sh -c "
 	sed -i 's/#.*$//;/^$/d' /etc/apt/sources.list
 
 	wget -q https://archive.neon.kde.org/public.key -O neon.key
-	if echo ee86878b3be00f5c99da50974ee7c5141a163d0e00fccb889398f1a33e112584 neon.key | sha256sum -c; then
 		apt-key add neon.key
 		echo deb http://archive.neon.kde.org/dev/stable xenial main >> /etc/apt/sources.list
 		echo deb http://archive.neon.kde.org/user xenial main >> /etc/apt/sources.list
-	fi
 	rm neon.key
 
 	wget -q http://repo.nxos.org/public.key -O nxos.key
-	if echo de7501e2951a9178173f67bdd29a9de45a572f19e387db5f4e29eb22100c2d0e nxos.key | sha256sum -c; then
 		apt-key add nxos.key
-		echo deb http://repo.nxos.org nxos main >> /etc/apt/sources.list
-		echo deb http://repo.nxos.org xenial main >> /etc/apt/sources.list
-	fi
+		echo deb http://repo.nxos.org/testing nxos main >> /etc/apt/sources.list
 	rm nxos.key
 
 	apt-get update
